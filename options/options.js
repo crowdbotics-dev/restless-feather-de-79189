ab4940fe469821eb50554b100295e2c3b97f3b10
Crowdbotics <engineering@crowdbotics.com>
2023-04-26 23:24:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-feather-de-79189",
+  url: "https://restless-feather-de-79189.botics.co",
+  api: "https://restless-feather-de-79189.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
